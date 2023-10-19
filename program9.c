226863ddd53230689ad8acf826c7da3f8f605df4
nusrat-rima <nusatrima3151@gmail.com>
2023-10-19 22:45:12 +0600

    printing from  m to n which is divisible by 3 oe 5

---
+++ program9.c
@@ -0,0 +1,23 @@
+// write a program that prints all the numbers
+// from m to n which is divisible by 3 or 5
+#include <stdio.h>
+int main()
+{
+    int count,m, n;
+
+     printf("enter m = ");
+    scanf("%d", &m);
+
+    printf("enter n = ");
+    scanf("%d", &n);
+
+    for (count = m; count <= n; count = count + 1)
+    {
+        if (count % 3 == 0 || count % 5 == 0)
+        {
+            printf("%d ", count);
+        }
+    }
+
+    return 0;
+}
