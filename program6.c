5d5cd2903bdd710aa4ed69b8dac8618e1381b83b
nusrat-rima <nusatrima3151@gmail.com>
2023-10-19 22:01:50 +0600

    printing even numbers

---
+++ program6.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    for (i=1; i<=10; i=i+1){
+        if(i%2==0){
+            printf("%d ", i);
+        }
+    }
+        
+    return 0;
+}
+// 1...10
+// output ->  2 4 6 8 10
