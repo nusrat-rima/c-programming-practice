448410e04bb73a92f8d71ed92abba917f6e7967a
nusrat-rima <nusatrima3151@gmail.com>
2023-10-20 14:46:19 +0600

    number swapping without using temp variable

--- program16.c
+++ program16.c
@@ -10,6 +10,6 @@ int main()
     printf(" num1= %d\n", num1);
     printf("num2 = %d\n", num2);
     return 0;
-    
+
 }
 
---
+++ program17.c
@@ -0,0 +1,15 @@
+//WAP in C number swapping without using temp variable 
+#include<stdio.h>
+int main()
+{
+
+    int num1 = 57;
+    int num2 = 33;
+    num1 = num1 - num2;
+    num2 = num1 + num2;
+    num1 = num2 - num1;
+    printf(" num1 = %d\n", num1);
+    printf(" num2 = %d\n", num2);
+    return 0;
+    
+}
