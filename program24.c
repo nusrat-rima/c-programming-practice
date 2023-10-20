44bee64fda30439e17dc286625550a76058f18dc
nusrat-rima <nusatrima3151@gmail.com>
2023-10-20 16:39:26 +0600

    printing hello world by using nested loop

---
+++ program24.c
@@ -0,0 +1,19 @@
+// print all the finoacci numbers from 1 to 100
+#include<stdio.h>
+int main()
+{
+    for (int i = 1; i<=5; i++){
+        for (int j = 1; j <= 3; j++) {
+                printf("hello world\n");
+        }
+    }
+    return 0;
+}
+
+/*i = 1 inner loop 3
+i = 2 inner loop 3
+i = 3 inner loop 3
+i = 4 inner loop 3
+i = 5 inner loop 3
+i = 6*/
+
