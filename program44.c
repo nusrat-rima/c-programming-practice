079dc65c4287267b1a618e8f848e9c967a112cb7
nusrat-rima <nusatrima3151@gmail.com>
2023-10-23 22:06:51 +0600

    introduction of array

---
+++ program44.c
@@ -0,0 +1,30 @@
+// datatype arrayname[arraysize]
+#include<stdio.h>
+int main()
+{
+    int grading[] = {20,30,40,50,60};
+    printf("%d", grading[4]);
+    // psh koro shunen na ..
+    // emergency baire jacchi por
+    // okey safely jaiyen
+    // i love you 
+    // i hate you more my dear husband
+
+    // i love you more bye
+
+    int numbers[5],
+    sum = 0; // numbers[0], numbers[1], numbers[2]. numbers[3], numbers[4]
+    numbers[0] = 10;
+    numbers[1] = 5;
+    numbers[2] = 67;
+    numbers[3] = 55;
+    numbers[4] = 37;
+    sum = numbers[0] + numbers[1] + numbers[2] + numbers[3] + numbers[4];
+    printf("sum = %d\n", sum);
+
+    // find the average and print the average
+    float average = (float)sum /5;
+    printf("average = %.2f\n", average);
+
+    return 0;
+}
