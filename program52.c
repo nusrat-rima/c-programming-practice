b1bc7df627aafde79b48c391f1747a9ad5e32033
nusrat-rima <nusatrima3151@gmail.com>
2023-10-25 00:44:58 +0600

    declaring and printing a matrix

---
+++ program52.c
@@ -0,0 +1,26 @@
+// multiplication of 2 matrix
+// decalaring and printing a matrix
+#include <stdio.h>
+int main()
+{
+    int a[2][4];
+
+    for (int row = 0; row < 2; row++){
+        for (int col = 0; col < 4; col++){
+            printf("\na[%d][%d]= ", row, col);
+            scanf("%d", &a[row][col]);
+        }
+    }
+
+
+    printf("a = \n");
+  
+    for (int row = 0; row < 2; row++){
+        for (int col = 0; col < 4; col++){
+             printf("%d ", a[row][col]);
+        }
+        printf("\n");
+    }
+
+        return 0;
+}
