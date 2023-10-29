4532c88f6b6622810302784c4ddc862ee1933a47
nusrat-rima <nusatrima3151@gmail.com>
2023-10-29 18:44:17 +0600

    factorial with using loop

---
+++ program63.c
@@ -0,0 +1,14 @@
+//factorial in loop
+#include<stdio.h>
+int main()
+{
+
+    int i, fact = 1, n;
+    printf("enter any positive number :");
+    scanf("%d",&n);
+    for (i = 1; i < n;i++){
+        fact = fact * i;
+    }
+    printf("%d\n", fact);
+    return 0;
+}
