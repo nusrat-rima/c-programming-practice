49a6792fc2506e0c3131c3ed40b269a35f73726c
nusrat-rima <nusatrima3151@gmail.com>
2023-10-29 22:12:57 +0600

    using string for counting  different things

--- program71.c
+++ program71.c
@@ -1,11 +1,11 @@
-//
+//using string for counting numbers of digit,vowels,consonant,special char
 #include<stdio.h>
 #include<string.h>
 int main()
 {
-    char text[] = "hello world. i am learning string. my phone number is 0406759900"; // string ends with '\0'
+    char text[] = "he 0406759900@"; // string ends with '\0'
 
-    int numOfVowel = 0, numOfConsonat=0,numOfDigits=0;
+    int numOfVowel = 0, numOfConsonat=0,numOfDigits=0,numOfSpecialchar=0,numOfSpaces=0;
     for (int i = 0; text[i] != '\0'; i++){
         if(text[i] == 'a' || text[i]=='e'|| text[i]=='i'|| text[i]=='o'||text[i]=='u'){
             numOfVowel++;
@@ -13,14 +13,25 @@ int main()
         else if(text[i]>='a' && text[i]<='z'){
             numOfConsonat++;
         }
-        else{
+        else if(text[i]>='0' && text[i]<='9'){
             numOfDigits++;
+        }
+        else if(text[i]==' '){
+            numOfSpaces++;
+        }
+        else{
+            numOfSpecialchar++;
         }
     }
     printf("number of vowels: %d\n", numOfVowel);
     printf("number of Consonants: %d\n", numOfConsonat);
     printf("number of Digits: %d\n", numOfDigits);
+    printf("number of Special char: %d\n", numOfSpecialchar);
+    printf("number of Spaces: %d\n",numOfSpaces);
+
+
 
 
     return 0;
 }
+
