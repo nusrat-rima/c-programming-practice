db446b6533397a8f926ce249210de8d4cb34cae9
nusrat-rima <148263929+nusrat-rima@users.noreply.github.com>
2023-10-30 13:20:51 +0600

    Area of a rectriangle in C

--- program14.c
+++ program14.c
@@ -1,11 +1,13 @@
-/
-
+//area of a rectriangle
+#include<stdio.h>
+int main()
+{
     float length, width, area;
     printf(" enter length = ");
     scanf("%f", &length);
     printf(" enter width = ");
     scanf("%f", &width);
     area = length * width;
-    printf(" area of a rectangle = %.2f\n",area);
+    printf(" area of a rectriangle = %.2f\n",area);
     return 0;
 }
