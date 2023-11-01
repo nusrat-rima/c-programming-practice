d9ac443b23a8c9b04198d6a6833ccc2dd2b1dd9c
nusrat-rima <nusatrima3151@gmail.com>
2023-11-01 23:51:30 +0600

    String Number Swapping

---
+++ program83.c
@@ -0,0 +1,18 @@
+//String Number Swapping
+#include<stdio.h>
+int main()
+{
+    char str1[15] = "Bangladesh";
+    char str2[15] = "Finland";
+    char temp[15];
+    printf("Before swapping\n");
+    printf("str1= %s\n", str1);
+    printf("str2= %s\n", str2);
+    strcpy(temp, str1);
+    strcpy(str1, str2);
+    strcpy(str2, temp);
+    printf("After swapping\n");
+    printf("str1= %s\n", str1);
+    printf("str2= %s\n", str2);
+    return 0;
+}
