7d1f3fe8e291c77e54d26d60f5434760a7b7b3d3
nusrat-rima <nusatrima3151@gmail.com>
2023-11-01 23:33:17 +0600

    Palindrome String Or not

---
+++ program82.c
@@ -0,0 +1,26 @@
+// Palindrome String Or not
+#include<stdio.h>
+int main()
+{
+    char str1[30] = "Nsrat Jahan Rima";
+    char str2[30];
+    int i = 0, j = 0, len = 0 ;
+    while(str1[i]!='\0'){
+        i++;
+        len++;
+    }
+    for (j = 0, i = len - 1; i >= 0; i--, j++)
+    {
+        str2[j] = str1[i];
+    }
+    str2[j] = '\0';
+    printf("str1= %s\n",str1);
+    printf("str2= %s\n",str2);
+    int compare = strcmp(str1,str2);
+    if(compare ==0){
+        printf("String is Palindrome");
+    }
+    else
+        printf("Not Palindrome");
+    return 0;
+}
