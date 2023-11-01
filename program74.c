ca81ea5ff6f37362c47715d5d79fd91f4de2a880
nusrat-rima <nusatrima3151@gmail.com>
2023-11-01 20:21:04 +0600

    Using strlen function for finding length

---
+++ program74.c
@@ -0,0 +1,10 @@
+//Using strlen function for finding length
+#include<stdio.h>
+int main()
+{
+    int len;
+    char s1[] = "Nusrat Jahan";
+    len = strlen(s1);
+    printf("length = %d\n", len);
+    return 0;
+}
