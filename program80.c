3eb139af4adcf47d323903be4ce1b8abda9fb7fa
nusrat-rima <nusatrima3151@gmail.com>
2023-11-01 22:18:08 +0600

    String reverse using strrev() function

---
+++ program80.c
@@ -0,0 +1,9 @@
+//String reverse using strrev() function
+#include<stdio.h>
+int main()
+{
+    char string[] = "Nusrat Jahan Rima";
+    strrev(string);
+    printf("string = %s\n", string);
+    return 0;
+}
